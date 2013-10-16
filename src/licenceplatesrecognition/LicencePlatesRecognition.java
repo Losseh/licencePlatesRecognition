698a055cab075e872962539719d54d5ee8a2a9f8
Adrian <aszymanski90@gmail.com>
2013-10-16 23:51:36 +0200

    Initial commit with files update

---
+++ src/licenceplatesrecognition/LicencePlatesRecognition.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package licenceplatesrecognition;
+
+/**
+ *
+ * @author adrian
+ */
+public class LicencePlatesRecognition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
